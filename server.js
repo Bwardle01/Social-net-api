b8dff5aea4266b1284570394019ff17936314b40
Bronson Wardle <bronson.wardle@gmail.com>
2023-04-11 16:50:54 -0400

    added mongoose package

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
 
 const PORT = process.env.port || 3001;
 const app = express();
-
 
 
 app.use(express.urlencoded({ extended: true }));
